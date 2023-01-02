b72b7ade600868e94b9e55cd813da1148f4bf718
FuadNabi <fuad.nebiyev95@mail.ru>
2023-01-02 15:17:57 +0400

    Add Navigation links

---
+++ src/components/MyProfile.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MyProfile = () => (
+  <main>
+    <h1>Lorem 2</h1>
+  </main>
+);
+
+export default MyProfile;
---
+++ src/components/Rockets.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Rockets = () => (
+  <main>
+    <h1>Lorem 3</h1>
+  </main>
+);
+
+export default Rockets;
