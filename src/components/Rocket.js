21df19ac9489bdd6a0303a5c84d8120f9507e4b2
FuadNabi <fuad.nebiyev95@mail.ru>
2023-01-04 12:42:41 +0400

    Add action to reserve button to make reserve true

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { reserveRockets } from '../redux/rocket/rocket';
 
 const Rocket = (props) => {
-  const { rocket: { name, image, description } } = props;
+  const {
+    rocket: {
+      id, name, image, description,
+    },
+  } = props;
+
+  const dispatch = useDispatch();
+  const reserve = () => {
+    dispatch(reserveRockets(id));
+  };
   return (
     <div className="rocket-container">
       <img alt="Rocket" src={image} className="rocket-img" />
       <div className="rocket-info">
         <h3 className="rocket-name">{name}</h3>
         <p className="rocket-description">{description}</p>
-        <button type="button" className="reserve-btn">Reserve Rocket</button>
+        <button type="button" className="reserve-btn" onClick={reserve}>Reserve Rocket</button>
       </div>
     </div>
   );
@@ -17,6 +28,7 @@ const Rocket = (props) => {
 
 Rocket.propTypes = {
   rocket: PropTypes.shape({
+    id: PropTypes.number,
     image: PropTypes.string,
     name: PropTypes.string,
     description: PropTypes.string,
