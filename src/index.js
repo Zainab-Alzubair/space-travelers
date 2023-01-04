15c8e7d514bc853f5e2609bbd143ec53833dc16b
Zainab Al-Zubair <70941894+Zainab-Alzubair@users.noreply.github.com>
2023-01-04 09:55:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import store from './redux/configureStore';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
