ad66266d49cd31dbd7c5e260df30473d77d0975e
FuadNabi <fuad.nebiyev95@mail.ru>
2023-01-05 11:55:54 +0400

    create function for Adding reserved names to user profile

--- src/components/pages/MyProfile.js
+++ src/components/pages/MyProfile.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const MyProfile = () => (
-  <main>
-    <h1>Lorem 2</h1>
-  </main>
-);
+const MyProfile = () => {
+  const rockets = useSelector((state) => state.rocketsReducer);
+  const reserved = rockets.filter((rocket) => rocket.reserved);
+
+  return (
+    <main>
+      <div>
+        <h1>My Rockets</h1>
+        <div>
+          {
+            reserved.map((rocket) => (
+              <div key={rocket.id} className="user-rockets">{rocket.name}</div>
+            ))
+          }
+        </div>
+      </div>
+    </main>
+  );
+};
 
 export default MyProfile;
